86f119b005caa3d85378e61cf16edd02b000a9e8
ninoerico <158866190+ninoerico@users.noreply.github.com>
2024-08-22 21:14:56 +0700

    Create welcome.java

---
+++ welcome.java
@@ -0,0 +1,26 @@
+/**
+  * This program displays a greeting for the reader.
+  * @version 1.30 2014-02-27
+  * @author Cay Horstmann
+  */
+
+ public class Welcome
+ {
+         public static void main(String[] args)
+ {
+         String greeting = "Welcome to Core Java!";
+ System.out.println(greeting);
+ for (int i = 0; i < greeting.length(); i++)
+        System.out.print("=");
+ System.out.println();
+ }
+
+         }
+// Permasalahan:
+// 1. Saat melakukan installasi intellij jaringan lambat sehingga memakan waktu yang cukup lama.
+// 2. Masih belum memahami cara bagaimana membuat file baru dengan meggunakan software intellij.
+// 3. Pada saat run program terdapat errror dan kesalahan pada systax sebelumnya.
+// Solusi 
+// 1. Menggunakan jaringan lan yang terdapat pada ruang praktik.
+// 2. Melakukan exolorasi fungsi-fungsi yang terdapat dapa tools dengan menggunakan  menonton video youtube.
+// 3. Melakukan cek pada script yang telah di tulis sebelumnya.
