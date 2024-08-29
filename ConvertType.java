f74d956d64a5a9661eb35e366566ec5911797462
ninoerico <158866190+ninoerico@users.noreply.github.com>
2024-08-29 23:02:38 +0700

    Update ConvertType.java

--- ConvertType.java
+++ ConvertType.java
@@ -1,43 +1,36 @@
-class ConvertDataType // deklarasi kelas
-{
-    static short methodOne(long l) {
-        //static short memiliki definisi untuk melakukan short dengan menggunakan methodOne
-        int i = (int) l;
-        // mengisi nilai dari integer i ke float
-        return (short)i; // melakukan pengulangan
-    }
-    public static void main(String[] args)
-    // deklarasi bahwa ini dapat dipanggil tanpa membuat class
-    {
-        double d = 10.25;
-        // nilai yang awalnya adalah bertipe double adalah 10.25
-        float f = (float) d;
-        // nilai di lakukan downcasting dari double ke float
-        byte b = (byte) methodOne((long) f);
-        // dari nilai mulai dri konfersi downcasting ini nilai dalam bentuk byte memanggil methodone long f
-        System.out.println(b);
-        // menampilkan ourput dari b yaitu nilai yang sebelumnya telah di downcasting dan di lakukan perubahan nilai
+import java.util.Scanner;
+public class ConvertType {
+    public static void main(String[] args) {
+        // Membuat objek Scanner untuk mengambil input dari pengguna
+        Scanner scanner = new Scanner(System.in);
+
+        // Membaca dua string A dan B dari input pengguna
+        String A = scanner.next();
+        String B = scanner.next();
+
+        // 1. Menghitung panjang dari A dan B, lalu menjumlahkannya
+        int lengthSum = A.length() + B.length();
+        // Mencetak hasil penjumlahan panjang A dan B
+        System.out.println(lengthSum);
+
+        // 2. Membandingkan A dan B secara leksikografis
+        if (A.compareTo(B) > 0) {
+            // Jika A lebih besar dari B secara leksikografis, cetak "Yes"
+            System.out.println("Yes");
+        } else {
+            // Jika A tidak lebih besar dari B, cetak "No"
+            System.out.println("No");
+        }
+
+        // 3. Mengkapitalisasi huruf pertama dari A dan B
+        // Mengambil huruf pertama dari A, mengubahnya menjadi huruf kapital,
+        // dan menggabungkannya dengan sisa huruf A
+        String capitalizedA = A.substring(0, 1).toUpperCase() + A.substring(1);
+
+        // Melakukan hal yang sama untuk B
+        String capitalizedB = B.substring(0, 1).toUpperCase() + B.substring(1);
+
+        // Mencetak A dan B yang sudah dikapitalisasi, dipisahkan dengan spasi
+        System.out.println(capitalizedA + " " + capitalizedB);
     }
 }
-//import java.util.Scanner;
-//public class ConvertDataType {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        String A = scanner.next();
-//        String B = scanner.next();
-//
-//        int lengthSum = A.length() + B.length();
-//        System.out.println(lengthSum);
-//
-//        if (A.compareTo(B) > 0) {
-//            System.out.println("Yes");
-//        } else {
-//            System.out.println("No");
-//        }
-//
-//        String capitalizedA = A.substring(0, 1).toUpperCase() + A.substring(1);
-//        String capitalizedB = B.substring(0, 1).toUpperCase() + B.substring(1);
-//        System.out.println(capitalizedA + " " + capitalizedB);
-//    }
-//}
